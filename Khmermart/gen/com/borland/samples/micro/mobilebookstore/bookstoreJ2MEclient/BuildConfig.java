7359596a249bdc4e555dbf2f73eae9032d38b10d
Gigithecode <querty@laposte.net>
2015-01-11 16:42:50 +0100

    Commit after name changed

---
+++ Khmermart/gen/com/borland/samples/micro/mobilebookstore/bookstoreJ2MEclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.borland.samples.micro.mobilebookstore.bookstoreJ2MEclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
